f07ce7f833ff4c7697176a8044ef4267b52ed326
evgenyivanov777 <elpo23@mail.ru>
2016-11-02 19:57:49 +0300

    Add files via upload

---
+++ lesson_4/lesson_4.1.js
@@ -0,0 +1,10 @@
+﻿"use strict"; 
+
+console.log(min(0, 10)); 
+console.log(min(0, -10)); 
+
+function min(a, b) { 
+        var minimum; 
+        minimum = (a <= b) ? a : b; 
+        return minimum; 
+}
---
+++ lesson_4/lesson_4.2.js
@@ -0,0 +1,18 @@
+﻿"use strict"; 
+
+function countBs(str) { 
+        return str.length 
+} 
+
+countBs('Пример') 
+
+//функция считает, сколько раз встречается символ в данной строке 
+function countChar(str, symbol) { 
+        var len = str.length, buf = i = 0; 
+        for (; i < len; i++) { 
+                if (str.charAt(i) == symbol) buf++; 
+        } 
+        return buf; 
+} 
+
+countChar('Good afternoon everybody', 'o'); 
