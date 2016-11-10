3d874e09c1c8ae42e91b8e1e22cf0499c7f1bce8
evgenyivanov777 <elpo23@mail.ru>
2016-11-10 22:25:10 +0300

    Вопрос

--- Question.js
+++ Question.js
@@ -6,3 +6,16 @@ list = {value:2, rest: list};
 //вариант 2
 var list = {};
 list.value = 2; list.rest = list;
+
+//и вот эта функия возвращает в итоге бесконечно вложенный друг в друга список,
+//хотя присваивается переменная buf, а не сам список list
+function arrayToList(a) {
+  var i = a.length, list = {}, buf = null;
+  for (; i > 0; i--) {
+    list.value = i;
+    list.rest = buf;
+    buf = list;
+  }
+  return list;
+
+arrayToList(arr);
